39ee5cd9daecc097ed8dc72f63eaff2894fdc9c8
Saptaji Kurniawan <saptaji.kurniawan@gmail.com>
2021-05-03 23:21:58 +0700

    a little change in file index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const coffeeStock = require('./state');
 
-console.log(coffeeStock);
+const makeCoffee = (type, milligrams) => {
+    if(coffeeStock[type] >= milligrams) {
+        console.log("Kopi berhasil dibuat");
+    } else {
+        console.log("Biji kopi habis");
+    }
+}
+
+makeCoffee("robusta", 180)
