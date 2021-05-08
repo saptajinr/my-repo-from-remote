65702c6538738dcefd966c2aa99af1bebb0d091a
Saptaji Kurniawan <saptaji.kurniawan@gmail.com>
2021-05-09 04:37:50 +0700

    add file throwingerrors.js and add some code to it.

--- throwingerrors.js
+++ throwingerrors.js
@@ -1,10 +1,24 @@
-let json = {"name": "Yoda", "age": 30};
+let json = '{"name": "Yoda", "age": 30}';
+//let json ='{bad json}';
+//let json = '{"age":20}';
 
 try {
     let user = JSON.parse(json);
-    console.log(user.name);
-    console.log(user.age);
+
+    if (!user.name) {
+        throw new SyntaxError("'name' is required.");        
+    } 
+    
+    errorCode;
+
+    console.log("Nama: "+user.name);
+    console.log("Umur: "+user.age);
 } catch (error) {
-    console.log(error.name);
-    console.log(error.message);
+    if (error instanceof SyntaxError) {
+        console.log(`JSON Error: ${error.message}`);
+    } else if (error instanceof ReferenceError) {
+        console.log(error.message);
+    } else {
+        console.log(error.stack);
+    }
 }
